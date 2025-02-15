957464a03416d7d81b46ca84a1ba9266c5a7a47b
ayush <Ayushsoni1221@gmail.com>
2025-02-15 11:57:45 +0530

    index update

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const bannerRoute = require("./routes/bannerRoute")
 const notificationRoute = require("./routes/notificationRoute")
 const blogRoute = require("./routes/blogRoute")
 const examRouter = require("./routes/examsRouting")
+const CourseRouter = require("./routes/course.route")
 
 dotenv.config()
-
+console.log("mongoUrl",process.env.MONGO_URL)
 connectDB().then(()=>console.log("db connected")).catch((err)=>console.log("err",err))
 
 const app = express();
@@ -30,6 +31,9 @@ app.use("/api/blog",blogRoute)
 
 //Exams
 app.use("/api/exams",examRouter)
+
+//Courses
+app.use("/api/courses",CourseRouter)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT , ()=>console.log(`server is listening on ${PORT}`))
